35521071eddc35b5f7f2c6e32753054da6a7caad
giuliocalamai14 <giuliocalamai94@gmail.com>
2017-03-09 14:43:09 +0100

    Revert "Negro"
    This reverts commit 3fe5f5274798332ae191128aa05ba75a196c2955.

--- js/index.js
+++ js/index.js
@@ -1,89 +1,9 @@
-<<<<<<< Updated upstream
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 43.88, lng: 11.55},
         zoom: 13
     });
     var infoWindow = new google.maps.InfoWindow({map: map});
-=======
-function initialize()
-{
-    if (navigator.geolocation)
-    {
-        navigator.geolocation.getCurrentPosition(displayLocation);
-    }
-    else
-    {
-        alert('Il tuo browser non supporta geolocalizzazione.');
-    }
-}
-function displayLocation (position)
-{
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-    // chiamiamo createMap con le coordinate della posizione individuata
-    createMap(position.coords);
-};
-
-
-function createMap(coords)
-{
-    var googleCoords = new google.maps.LatLng(coords.latitude, coords.longitude);
-
-    var myOptions =
-    {
-        zoom: 16,
-        center: googleCoords,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR},
-        styles:[
-            {
-                "featureType": "road",
-                "elementType": "geometry.stroke",
-                "stylers": [
-                    {
-                        "hue": "#007dff"
-                    },
-                    {
-                        "visibility": "on"
-                    }
-                ]
-            },
-            {
-                "featureType": "road",
-                "elementType": "labels",
-                "stylers": [
-                    {
-                        "hue": "#ff8800"
-                    },
-                    {
-                        "visibility": "on"
-                    }
-                ]
-            },
-            {
-                "featureType": "road.highway",
-                "elementType": "geometry.fill",
-                "stylers": [
-                    {
-                        "visibility": "on"
-                    }
-                ]
-            },
-            {
-                "featureType": "road.highway",
-                "elementType": "geometry.stroke",
-                "stylers": [
-                    {
-                        "visibility": "on"
-                    },
-                    {
-                        "hue": "#edff00"
-                    }
-                ]
-            }
-        ]
->>>>>>> Stashed changes
 
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
@@ -104,7 +24,6 @@ function createMap(coords)
     }
 }
 
-<<<<<<< Updated upstream
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
@@ -112,8 +31,6 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         'Error: Your browser doesn\'t support geolocation.');
 }
 
-=======
->>>>>>> Stashed changes
 function flagIta()
 {
     document.getElementById('ita').src='img/italia.png';
